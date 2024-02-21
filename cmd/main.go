475cb89fe982b0207b7fb64284ac7962681631cc
PELENIUM <i.d.chernyshov@yandex.ru>
2024-02-21 23:31:44 +0300

    added db

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 	"technostrelka/internal/handlers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db, err := sql.Open("sqlite3", "./internal/db/users.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	r := gin.Default()
 
 	r.LoadHTMLGlob("./internal/static/html/*.html")
@@ -14,8 +22,8 @@ func main() {
 	r.GET("/", handlers.Index)
 
 	r.GET("/login", handlers.Login)
-	r.GET("/signup", handlers.Login)
-	r.GET("/profile", handlers.Login)
+	r.GET("/signup", handlers.Signup)
+	// r.GET("/profile", handlers.Login)
 
 	r.Run(":8080")
 }
